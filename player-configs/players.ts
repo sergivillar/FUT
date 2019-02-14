658d4f4803475e6bf3a02c740b3016f65048d33f
Sergio Fernández <sergio.fernandezj@gmail.com>
2019-02-14 09:20:48 +0100

    added players config

--- player-configs/players.ts
+++ player-configs/players.ts
@@ -1,10 +1,10 @@
-import {GOLD, PlayerConfig} from '../src/models';
+import {GOLD, SPECIAL, PlayerConfig} from '../src/models';
 
 const players: PlayerConfig[] = [
     {
-        name: 'Jorge Meré',
+        name: 'José María Giménez',
         quality: GOLD,
-        rating: 77,
+        rating: 84,
         bid: {
             minBidPrice: 0,
             maxBidPrice: 350,
@@ -13,9 +13,117 @@ const players: PlayerConfig[] = [
             maxExpirationTime: 4600,
         },
         buyNow: {
-            maxBuyNowPrice: 350,
-            maxIterations: 100,
-            playersToBuy: 1,
+            maxBuyNowPrice: 3000,
+            maxIterations: 150,
+            playersToBuy: 5,
+        },
+        sell: {price: 100},
+    },
+    {
+        name: 'Carvajal',
+        quality: GOLD,
+        rating: 84,
+        bid: {
+            minBidPrice: 0,
+            maxBidPrice: 350,
+            minBuyNowPrice: 0,
+            maxBuyNowPrice: 0,
+            maxExpirationTime: 4600,
+        },
+        buyNow: {
+            maxBuyNowPrice: 3000,
+            maxIterations: 150,
+            playersToBuy: 5,
+        },
+        sell: {price: 100},
+    },
+    {
+        name: 'Koke',
+        quality: GOLD,
+        rating: 85,
+        bid: {
+            minBidPrice: 0,
+            maxBidPrice: 350,
+            minBuyNowPrice: 0,
+            maxBuyNowPrice: 0,
+            maxExpirationTime: 4600,
+        },
+        buyNow: {
+            maxBuyNowPrice: 10000,
+            maxIterations: 150,
+            playersToBuy: 5,
+        },
+        sell: {price: 100},
+    },
+    {
+        name: 'Marco Asensio',
+        quality: GOLD,
+        rating: 85,
+        bid: {
+            minBidPrice: 0,
+            maxBidPrice: 350,
+            minBuyNowPrice: 0,
+            maxBuyNowPrice: 0,
+            maxExpirationTime: 4600,
+        },
+        buyNow: {
+            maxBuyNowPrice: 10000,
+            maxIterations: 150,
+            playersToBuy: 5,
+        },
+        sell: {price: 100},
+    },
+    {
+        name: 'Miranda',
+        quality: GOLD,
+        rating: 85,
+        bid: {
+            minBidPrice: 0,
+            maxBidPrice: 350,
+            minBuyNowPrice: 0,
+            maxBuyNowPrice: 0,
+            maxExpirationTime: 4600,
+        },
+        buyNow: {
+            maxBuyNowPrice: 10000,
+            maxIterations: 150,
+            playersToBuy: 5,
+        },
+        sell: {price: 100},
+    },
+    {
+        name: 'Saúl',
+        quality: GOLD,
+        rating: 85,
+        bid: {
+            minBidPrice: 0,
+            maxBidPrice: 350,
+            minBuyNowPrice: 0,
+            maxBuyNowPrice: 0,
+            maxExpirationTime: 4600,
+        },
+        buyNow: {
+            maxBuyNowPrice: 10000,
+            maxIterations: 150,
+            playersToBuy: 5,
+        },
+        sell: {price: 100},
+    },
+    {
+        name: 'Pizzi',
+        quality: SPECIAL,
+        rating: 85,
+        bid: {
+            minBidPrice: 0,
+            maxBidPrice: 350,
+            minBuyNowPrice: 0,
+            maxBuyNowPrice: 0,
+            maxExpirationTime: 4600,
+        },
+        buyNow: {
+            maxBuyNowPrice: 24000,
+            maxIterations: 150,
+            playersToBuy: 5,
         },
         sell: {price: 100},
     },
