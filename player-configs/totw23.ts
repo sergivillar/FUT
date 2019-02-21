659252c172c0733f2d8914cd10c9c1bef818d324
César Estébanez Tascón <cestebanez@gmail.com>
2019-02-21 10:30:02 +0100

    TOTW23 massive bidding config

---
+++ player-configs/totw23.ts
@@ -0,0 +1,90 @@
+import {SPECIAL, PlayerConfig} from '../src/models';
+
+const players: PlayerConfig[] = [
+    {
+        name: 'Oyarzabal',
+        quality: SPECIAL,
+        rating: 83,
+        bid: {
+            minBidPrice: 0,
+            maxBidPrice: 18000,
+            minBuyNowPrice: 11500,
+            maxBuyNowPrice: 0,
+            maxExpirationTime: 600,
+        },
+    },
+    {
+        name: 'Portu',
+        quality: SPECIAL,
+        rating: 83,
+        bid: {
+            minBidPrice: 0,
+            maxBidPrice: 18000,
+            minBuyNowPrice: 11500,
+            maxBuyNowPrice: 0,
+            maxExpirationTime: 600,
+        },
+    },
+    {
+        name: 'Domenico Criscito',
+        quality: SPECIAL,
+        rating: 84,
+        bid: {
+            minBidPrice: 0,
+            maxBidPrice: 23000,
+            minBuyNowPrice: 11500,
+            maxBuyNowPrice: 0,
+            maxExpirationTime: 600,
+        },
+    },
+    {
+        name: 'Álvaro',
+        quality: SPECIAL,
+        rating: 84,
+        bid: {
+            minBidPrice: 0,
+            maxBidPrice: 23000,
+            minBuyNowPrice: 11500,
+            maxBuyNowPrice: 0,
+            maxExpirationTime: 600,
+        },
+    },
+    {
+        name: 'Marçal',
+        quality: SPECIAL,
+        rating: 81,
+        bid: {
+            minBidPrice: 0,
+            maxBidPrice: 13000,
+            minBuyNowPrice: 11500,
+            maxBuyNowPrice: 0,
+            maxExpirationTime: 600,
+        },
+    },
+    {
+        name: 'Rochina',
+        quality: SPECIAL,
+        rating: 81,
+        bid: {
+            minBidPrice: 0,
+            maxBidPrice: 13000,
+            minBuyNowPrice: 11500,
+            maxBuyNowPrice: 0,
+            maxExpirationTime: 600,
+        },
+    },
+    {
+        name: 'Sofiane Feghouli',
+        quality: SPECIAL,
+        rating: 82,
+        bid: {
+            minBidPrice: 0,
+            maxBidPrice: 13000,
+            minBuyNowPrice: 11500,
+            maxBuyNowPrice: 0,
+            maxExpirationTime: 600,
+        },
+    },
+];
+
+export default players;
